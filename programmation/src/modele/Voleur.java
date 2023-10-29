4465a3c5974045c0eeb3bb6d306503f4eadf0098
matthisisback <teplitxkymatthis@gmail.com>
2023-10-29 12:38:58 +0100

    changement esthétique

--- programmation/src/modele/Voleur.java
+++ programmation/src/modele/Voleur.java
@@ -3,24 +3,26 @@ package modele;
 import controleur.Interaction;
 
 public class Voleur extends Personnage {
-	
+
 	private boolean vole = false;
-	
+
 	public Voleur() {
-		super("Voleur",2,Caracteristiques.VOLEUR);
+		super("Voleur", 2, Caracteristiques.VOLEUR);
 	}
-	
+
 	@Override
 	public void utiliserPouvoir() {
 		System.out.println("Quel personnage voulez-vous volez ? ");
-		for(int i = 0; i < getPlateau().getNombrePersonnages(); i++) { //TODO : Faire qu'un message apparaisse s'il se cible lui même et enlever les chiffres degeu si possible
-			System.out.println(i+1 + " " + getPlateau().getPersonnage(i));
+		for (int i = 0; i < getPlateau().getNombrePersonnages(); i++) { // TODO : Faire qu'un message apparaisse s'il se
+																		// cible lui même et enlever les chiffres degeu
+																		// si possible
+			System.out.println(i + 1 + " " + getPlateau().getPersonnage(i).getNom());
 		}
-		while(!vole) {
+		while (!vole) {
 			System.out.print("Votre choix : ");
-			int id = Interaction.lireUnEntier(1,getPlateau().getNombrePersonnages());
-			Personnage cible = getPlateau().getPersonnage(id-1);
-			if(cible.getNom() == "Voleur" || cible.getRang()==1) {
+			int id = Interaction.lireUnEntier(1, getPlateau().getNombrePersonnages());
+			Personnage cible = getPlateau().getPersonnage(id - 1);
+			if (cible.getNom() == "Voleur" || cible.getRang() == 1) {
 				System.out.println("Vous ne pouvez pas voler ce personnage.");
 			} else {
 				vole = true;
