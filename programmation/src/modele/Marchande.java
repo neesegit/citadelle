497e66e9e64199b1b2facb39641fe57fb97abb9b
matthisisback <teplitxkymatthis@gmail.com>
2023-10-30 15:01:05 +0100

    Classe Marchande plus test réussi

---
+++ programmation/src/modele/Marchande.java
@@ -0,0 +1,39 @@
+package modele;
+
+public class Marchande extends Personnage {
+    private int compteur = 0;
+
+    public Marchande() {
+        super("Marchande", 6, Caracteristiques.MARCHANDE);
+    }
+
+    @Override
+    public void percevoirRessourcesSpecifiques() {
+        if (getJoueur().quartierPresentDansCite("taverne")) {
+            compteur++;
+        }
+        if (getJoueur().quartierPresentDansCite("échoppe")) {
+            compteur++;
+        }
+        if (getJoueur().quartierPresentDansCite("marché")) {
+            compteur++;
+        }
+        if (getJoueur().quartierPresentDansCite("comptoir")) {
+            compteur++;
+        }
+        if (getJoueur().quartierPresentDansCite("port")) {
+            compteur++;
+        }
+        if (getJoueur().quartierPresentDansCite("hôtel de ville")) {
+            compteur++;
+        }
+        this.getJoueur().ajouterPieces(compteur);
+        System.out.println("Ajouter " + compteur + " pièce" + (compteur > 1 ? "s" : ""));
+    }
+
+    @Override
+    public void utiliserPouvoir() {
+        this.getJoueur().ajouterPieces(1);
+        System.out.println("Ajouter 1 pièce");
+    }
+}
