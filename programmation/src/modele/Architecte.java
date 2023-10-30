ad900068286509673784d75280c299ae08e53688
matthisisback <teplitxkymatthis@gmail.com>
2023-10-30 15:20:40 +0100

    Classe Architecte plus test réussi

---
+++ programmation/src/modele/Architecte.java
@@ -0,0 +1,14 @@
+package modele;
+
+public class Architecte extends Personnage {
+
+    public Architecte() {
+        super("Architecte", 6, Caracteristiques.ARCHITECTE);
+    }
+
+    @Override
+    public void utiliserPouvoir() {
+        getJoueur().ajouterQuartierDansMain(getPlateau().getPioche().piocher());
+        getJoueur().ajouterQuartierDansMain(getPlateau().getPioche().piocher());
+    }
+}
