180185d03ee13614e74274746299e727799e46f9
matthisisback <teplitxkymatthis@gmail.com>
2023-10-30 14:49:19 +0100

    Classe Eveque plus test réussi et petit changement de roi

---
+++ programmation/src/modele/Eveque.java
@@ -0,0 +1,33 @@
+package modele;
+
+public class Eveque extends Personnage {
+    private int compteur = 0;
+
+    public Eveque() {
+        super("Eveque", 5, Caracteristiques.EVEQUE);
+    }
+
+    @Override
+    public void percevoirRessourcesSpecifiques() {
+        if (getJoueur().quartierPresentDansCite("temple")) {
+            compteur++;
+        }
+        if (getJoueur().quartierPresentDansCite("église")) {
+            compteur++;
+        }
+        if (getJoueur().quartierPresentDansCite("monastère")) {
+            compteur++;
+        }
+        if (getJoueur().quartierPresentDansCite("cathédrale")) {
+            compteur++;
+        }
+        this.getJoueur().ajouterPieces(compteur);
+        System.out.println("Ajouter " + compteur + " pièce" + (compteur > 1 ? "s" : ""));
+    }
+
+    @Override
+    public void utiliserPouvoir() {
+        // attention pas dans son pouvoir mais dans les personnage de rang 8 il est
+        // protéger
+    }
+}
--- programmation/src/modele/Roi.java
+++ programmation/src/modele/Roi.java
@@ -4,27 +4,36 @@ public class Roi extends Personnage {
     private int compteur = 0;
 
     public Roi() {
-        super("Roi",4,Caracteristiques.ROI);
+        super("Roi", 4, Caracteristiques.ROI);
 
     }
+
     @Override
     public void utiliserPouvoir() {
         System.out.println("Je prends la couronne");
-        if (getJoueur()!=null) {
+        if (getJoueur() != null) {
             this.getJoueur().setPossedeCouronne(true);
         }
     }
+
     public void percevoirRessourcesSpecifiques() {
         if (this.getJoueur() == null) {
             System.out.println("Ce joueur n'est pas roi");
             return;
         }
-        //il y a déja une boucle for dans quartierPresentDansCite
-        // le nom des quartiers c'est pas le type attention donc on fait un ou avec les trois quartiers noble
-        if(this.getJoueur().quartierPresentDansCite("manoir")||this.getJoueur().quartierPresentDansCite("château")||this.getJoueur().quartierPresentDansCite("palais")) {
+        // il y a déja une boucle for dans quartierPresentDansCite
+        // le nom des quartiers c'est pas le type attention donc on fait un ou avec les
+        // trois quartiers noble
+        if (this.getJoueur().quartierPresentDansCite("manoir")) {
+            compteur++;
+        }
+        if (this.getJoueur().quartierPresentDansCite("château")) {
+            compteur++;
+        }
+        if (this.getJoueur().quartierPresentDansCite("palais")) {
             compteur++;
         }
         this.getJoueur().ajouterPieces(compteur);
-        System.out.println("Ajouter "+compteur+" pièce"+(compteur>1?"s":""));
+        System.out.println("Ajouter " + compteur + " pièce" + (compteur > 1 ? "s" : ""));
     }
 }
