feb1c6223f9c58684d9f35f8f49b50881d8b4596
matthisisback <teplitxkymatthis@gmail.com>
2023-12-11 21:03:32 +0100

    Fini la classe Empereur et son test le dernier test a faire plus tard

--- programmation/src/modele/Empereur.java
+++ programmation/src/modele/Empereur.java
@@ -6,10 +6,10 @@ import controleur.Interaction;
 
 public class Empereur extends Personnage {
 
-    private String[] noble = {"manoir","château","palais"};
+    private String[] noble = { "manoir", "château", "palais" };
 
     private PlateauDeJeu plateau;
-    private Personnage cible;
+    private Joueur cible;
     private String choix;
 
     private boolean choixBot = true;
@@ -19,14 +19,16 @@ public class Empereur extends Personnage {
     public Empereur() {
         super("Empereur", 4, Caracteristiques.EMPEREUR);
         this.plateau = getPlateau();
-        if(this.plateau == null) plateau = new PlateauDeJeu();
+        if (this.plateau == null)
+            plateau = new PlateauDeJeu();
     }
 
     @Override
-    public void percevoirRessourcesSpecifiques(){
+    public void percevoirRessourcesSpecifiques() {
         // 1 piece d'or pour chaque quartier noble dans sa cite
-        for(int i = 0; i < noble.length; i++){
-            if (this.getJoueur().quartierPresentDansCite(noble[i])) this.getJoueur().ajouterPieces(1);
+        for (int i = 0; i < noble.length; i++) {
+            if (this.getJoueur().quartierPresentDansCite(noble[i]))
+                this.getJoueur().ajouterPieces(1);
         }
     }
 
@@ -34,46 +36,72 @@ public class Empereur extends Personnage {
     public void utiliserPouvoir() {
         // Donne couronne contre 1 pièce ou 1 carte
         System.out.println("A quel personnage voulez-vous donner la couronne ?");
-        for (int i = 0; i < getPlateau().getNombrePersonnages(); i++) {
-            System.out.println(i + 1 + " " + getPlateau().getPersonnage(i).getNom() + " " + getJoueur().nbPieces());
+        for (int i = 0; i < getPlateau().getNombreJoueurs(); i++) {
+            System.out.println(i + 1 + " " + getPlateau().getJoueur(i).getNom() + " ("
+                    + getPlateau().getJoueur(i).getPersonnage().getNom() + ") piece"
+                    + (getPlateau().getJoueur(i).nbPieces() > 1 ? "s" : "") + " :"
+                    + getPlateau().getJoueur(i).nbPieces() + " nombre de carte"
+                    + (getPlateau().getJoueur(i).nbQuartiersDansMain() > 1 ? "s" : "") + " :"
+                    + getPlateau().getJoueur(i).nbQuartiersDansMain());
         }
-        System.out.println("Votre choix : ");
-        int id = Interaction.lireUnEntier(1, getPlateau().getNombrePersonnages() + 1);
-        cible = getPlateau().getPersonnage(id - 1);
-        this.getJoueur().setPossedeCouronne(false);
-        cible.getJoueur().setPossedeCouronne(true);
-
-        System.out.print("Voulez vous une (p)ièce ou une (c)arte ? ");
-        choix = Interaction.lireUneChaine();
-        if(choix.equals("p")){
-            this.getJoueur().ajouterPieces(1);
-            cible.getJoueur().retirerPieces(1);
-        } else if(choix.equals("c")){
-            this.getJoueur().ajouterQuartierDansMain(cible.getJoueur().retirerQuartierDansMain());
+        boolean pasmoi = true;
+        while (pasmoi) {
+            System.out.println("Votre choix (autre que vous et qui n'as pas deja la courronne): ");
+            int id = Interaction.lireUnEntier(1, getPlateau().getNombreJoueurs() + 1);
+            cible = getPlateau().getJoueur(id - 1);
+            if (cible != this.getJoueur() && !(cible.getPossedeCouronne())) {
+                pasmoi = false;
+            }
         }
-
+        for (int i = 0; i < getPlateau().getNombreJoueurs(); i++) {
+            if (getPlateau().getJoueur(i).getPossedeCouronne()) {
+                getPlateau().getJoueur(i).setPossedeCouronne(false);
+            }
+        }
+        cible.setPossedeCouronne(true);
+        boolean reponse = false;
+        while (!reponse) {
+            System.out.print("Voulez vous une (p)ièce ou une (c)arte ? ");
+            choix = Interaction.lireUneChaine();
+            if (choix.equals("p")) {
+                reponse = true;
+                if (cible.nbPieces() > 0) {
+                    this.getJoueur().ajouterPieces(1);
+                    cible.retirerPieces(1);
+                } else {
+                    System.out.println("La cible n'a pas de piece");
+                }
+            } else if (choix.equals("c")) {
+                reponse = true;
+                if (cible.nbQuartiersDansMain() > 0) {
+                    this.getJoueur().ajouterQuartierDansMain(cible.retirerQuartierDansMain());
+                } else {
+                    System.out.println("La cible n'a pas de carte quartier");
+                }
+            }
+        }
     }
 
     @Override
     public void utiliserPouvoirAvatar() {
         // pareil mais avec un bot
 
-        while(choixBot){
-            int id = random.nextInt(this.plateau.getNombrePersonnages())+1;
-            cible = this.plateau.getPersonnage(id-1);
-            if(cible.getNom() != "Empereur") choixBot = false;
+        while (choixBot) {
+            int id = random.nextInt(this.plateau.getNombrePersonnages()) + 1;
+            cible = this.plateau.getPersonnage(id - 1);
+            if (cible.getNom() != "Empereur")
+                choixBot = false;
         }
 
         this.getJoueur().setPossedeCouronne(false);
         cible.getJoueur().setPossedeCouronne(true);
 
         boolean choixPCBot = random.nextBoolean();
-        if(choixPCBot){
+        if (choixPCBot) {
             this.getJoueur().ajouterPieces(1);
             cible.getJoueur().retirerPieces(1);
         } else {
             this.getJoueur().ajouterQuartierDansMain(cible.getJoueur().retirerQuartierDansMain());
         }
     }
-    
 }
