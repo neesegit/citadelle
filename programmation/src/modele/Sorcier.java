c93bb67aba00ec6494f0d5b5c1cb9cdbccc0e650
Mattieu <mattieu.languereau@gmail.com>
2023-12-12 18:45:33 +0100

    Modif Echevin Ajout Sorcier

---
+++ programmation/src/modele/Sorcier.java
@@ -0,0 +1,65 @@
+package modele;
+
+import controleur.Interaction;
+
+public class Sorcier extends Personnage{
+
+    private int idJoueur = 0;
+    private Joueur joueur;
+
+    private int idCarte = 0;
+    private Quartier quartier;
+
+    private boolean choix;
+
+    public Sorcier() {
+        super("Sorcier", 3, Caracteristiques.SORCIER);
+    }
+
+    @Override
+    public void utiliserPouvoir() {
+
+        System.out.println("De quel joueur souhaitez vous choisir la carte ?");
+        for(int i = 0; i<getPlateau().getNombreJoueurs();i++){
+            System.out.println(i+1 + " " + getPlateau().getJoueur(i).getNom());
+        }
+
+        System.out.print("Votre choix : ");
+        idJoueur = Interaction.lireUnEntier(1, getPlateau().getNombreJoueurs())-1;
+
+        joueur = getPlateau().getJoueur(idJoueur);
+
+        System.out.println("Quel carte souhaitez vous choisir ?");
+        for(int i = 0; i<joueur.getMain().size();i++){
+            System.out.println(i+1 + " " + joueur.getMain().get(i).getNom());
+        }
+
+        System.out.print("Votre choix : ");
+        idCarte = Interaction.lireUnEntier(1, joueur.getMain().size());
+
+        quartier = joueur.getMain().get(idCarte);
+
+        System.out.println("Souhaitez vous prendre la carte (oui) ou la construire (non)?");
+        choix = Interaction.lireOuiOuNon();
+
+        if(choix){
+            //prendre la carte
+            this.getJoueur().ajouterQuartierDansMain(quartier);
+        } else {
+            //construire la carte
+            if (this.getJoueur() == null || this.getAssassine() == true) {
+                return;
+            }
+            if (this.getJoueur().nbPieces() < quartier.coutConstruction) {
+                System.out.println("Vous n'avez pas  d'or pour construire ce quartier");
+                return;
+            }
+            this.getJoueur().ajouterQuartierDansCite(quartier);
+            this.getJoueur().retirerPieces(quartier.coutConstruction);
+        }
+    }
+
+    @Override
+    public void utiliserPouvoirAvatar() {}
+    
+}
