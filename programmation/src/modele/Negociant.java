943f73b3b7d584a40cba9152cb0bc274ecfe7311
matthisisback <teplitxkymatthis@gmail.com>
2023-12-12 11:35:28 +0100

    Class Negociant et TestNegociant

--- programmation/src/modele/Negociant.java
+++ programmation/src/modele/Negociant.java
@@ -1,8 +1,8 @@
 package modele;
 
-public class Negociant extends Personnage{
+public class Negociant extends Personnage {
 
-    private String[] commercant = {"taverne", "échoppe", "marché", "comptoir", "port", "hôtel de ville"};
+    private String[] commercant = { "taverne", "échoppe", "marché", "comptoir", "port", "hôtel de ville" };
 
     private boolean isCommercant = false;
 
@@ -11,50 +11,63 @@ public class Negociant extends Personnage{
     }
 
     @Override
-    public void percevoirRessourcesSpecifiques(){
-        for(int i = 0; i < commercant.length; i++){
-            if (this.getJoueur().quartierPresentDansCite(commercant[i])) this.getJoueur().ajouterPieces(1);
+    public void percevoirRessourcesSpecifiques() {
+        for (int i = 0; i < commercant.length; i++) {
+            if (this.getJoueur().quartierPresentDansCite(commercant[i]))
+                this.getJoueur().ajouterPieces(1);
         }
     }
+
     @Override
-    public void construire(Quartier nouveau){
-
-        for(int i = 0; i < commercant.length; i++){
-            if(nouveau.getType() == commercant[i]) isCommercant = true;
+    public void construire(Quartier nouveau) {
+        isCommercant = false;
+        if (nouveau.getType().equals("COMMERCANT")) {
+            isCommercant = true;
+            ;
         }
-
-        if (this.getDejaConstruit()) {
-            if(isCommercant){
-                if (this.getJoueur() == null || this.getAssassine() == true) {
-                    return;
-                }
-                if (this.getJoueur().nbPieces() < nouveau.coutConstruction) {
-                    System.out.println("Vous n'avez pas  d'or pour construire ce quartier");
-                    return;
-                }
-                this.getJoueur().ajouterQuartierDansCite(nouveau);
-                this.setDejaConstruit(false);
-            } else {
-                if (this.getJoueur() == null || this.getAssassine() == true) {
-                    return;
-                }
-                if (this.getJoueur().nbPieces() < nouveau.coutConstruction) {
-                    System.out.println("Vous n'avez pas  d'or pour construire ce quartier");
-                    return;
-                }
-                this.getJoueur().ajouterQuartierDansCite(nouveau);
-                this.getJoueur().retirerPieces(nouveau.coutConstruction);
-                this.setDejaConstruit(false);
+        if (isCommercant) {
+            if (this.getJoueur() == null || this.getAssassine() == true) {
+                return;
             }
-            System.out.println("Vous construit un quartier dans ce tour");
+            if (this.getJoueur().quartierPresentDansCite(nouveau.getNom())) {
+                System.out.println("Vous avez déja le meme quartier dans votre cite");
+                return;
+            }
+            if (this.getJoueur().nbPieces() < nouveau.coutConstruction) {
+                System.out.println("Vous n'avez pas  d'or pour construire ce quartier");
+                return;
+            }
+            this.getJoueur().ajouterQuartierDansCite(nouveau);
+            this.getJoueur().retirerPieces(nouveau.coutConstruction);
+            this.setDejaConstruit(true);
+            System.out.println("Vous avez construit un quartier commercant dans ce tour");
+            return;
         }
-
+        if (this.getDejaConstruit()) {
+            if (this.getJoueur() == null || this.getAssassine() == true) {
+                return;
+            }
+            if (this.getJoueur().quartierPresentDansCite(nouveau.getNom())) {
+                System.out.println("Vous avez déja le meme quartier dans votre cite");
+                return;
+            }
+            if (this.getJoueur().nbPieces() < nouveau.coutConstruction) {
+                System.out.println("Vous n'avez pas  d'or pour construire ce quartier");
+                return;
+            }
+            this.getJoueur().ajouterQuartierDansCite(nouveau);
+            this.getJoueur().retirerPieces(nouveau.coutConstruction);
+            this.setDejaConstruit(false);
+            System.out.println("Vous avez construit un quartier dans ce tour");
+        }
     }
 
     @Override
-    public void utiliserPouvoir() {}
+    public void utiliserPouvoir() {
+    }
 
     @Override
-    public void utiliserPouvoirAvatar() {}
-    
+    public void utiliserPouvoirAvatar() {
+    }
+
 }
